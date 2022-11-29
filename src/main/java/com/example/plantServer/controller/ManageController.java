1e93eacaea0a94930fb84ed74ec570a1940e7fb0
kk210001 <91827665+kk210001@users.noreply.github.com>
2022-11-29 23:10:28 +0900

    pot 상태를 웹에서 관리하기 위한 페이지 condition.html 과 업데이트 기능 제외한 조회, 삭제, 추가 기능 가진 ManageController 추가

---
+++ src/main/java/com/example/plantServer/controller/ManageController.java
@@ -0,0 +1,54 @@
+package com.example.plantServer.controller;
+
+import com.example.plantServer.entity.Pot;
+import com.example.plantServer.respository.PotRepository;
+import com.example.plantServer.respository.WateringLogRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class ManageController {
+    private final PotRepository potRepository;
+    private final WateringLogRepository wateringLogRepository;
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        List<Pot> potList = new ArrayList<>();
+        potRepository.findAll().forEach(potList::add);
+        model.addAttribute("pots", potList);
+
+        log.info("list={}",potList);
+        return "manage/condition";
+
+    }
+    @GetMapping("/delete/{serialId}")
+    public String deleteSerialId(@PathVariable("serialId") String serialId) {
+        potRepository.deleteBySerialId(serialId);
+        log.info("serialId deleted = {}", serialId);
+        return "redirect:/condition";
+    }
+    @GetMapping("/add")
+    public String addSerialId() {
+
+            String serialNumber = UUID.randomUUID().toString();
+
+            Pot addPot = new Pot();
+            addPot.setSerialId(serialNumber);
+
+            potRepository.save(addPot);
+
+            return "redirect:/pot/init";
+
+    }
+
+}
